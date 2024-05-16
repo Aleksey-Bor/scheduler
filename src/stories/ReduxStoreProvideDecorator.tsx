94fed9e3cb9289d3144a87332184f68779c1c0c7
Aleksey Borkovsky <it.borki@gmail.com>
2024-05-16 19:17:49 +0300

    Added history for the App component

--- src/stories/ReduxStoreProvideDecorator.tsx
+++ src/stories/ReduxStoreProvideDecorator.tsx
@@ -1,6 +1,54 @@
 import { Provider } from "react-redux"
-import store from "../state/store"
+import { configureStore } from '@reduxjs/toolkit'
+import { tasksReducer } from '../state/tasks-reducer'
+import { todoListsReducer } from '../state/totolists-reducer'
+import { v1 } from "uuid"
+import { TodoListType } from "../App"
+import { TaskType } from "../TodoList"
+
+
+type InitialStateType = {
+  todoLists: Array<TodoListType>;
+  tasks: Record<string, Array<TaskType>>;
+}
+
+const todoListId1 = v1();
+const todoListId2 = v1();
+const todoListId3 = v1();
+
+const initialGlobalState: InitialStateType = {
+  todoLists: [
+    {id: todoListId1, title: "Что купить", filter: "all"},
+    {id: todoListId2, title: "Что изучить", filter: "completed"},
+    {id: todoListId3, title: "Что посмотреть", filter: "active"},
+  ],
+  tasks: {
+    [todoListId1]: [
+      {id: v1(), title: "Молоко", isDone: false},
+      {id: v1(), title: "Хлеб", isDone: false},
+    ],
+    [todoListId2]: [
+      {id: v1(), title: "HTML", isDone: true},
+      {id: v1(), title: "CSS", isDone: true},
+      {id: v1(), title: "JS", isDone: true},
+      {id: v1(), title: "React", isDone: false},
+      {id: v1(), title: "Redux", isDone: false},
+    ],
+    [todoListId3]: [
+      {id: v1(), title: "Силиконовая долина (последний сезон)", isDone: true},
+      {id: v1(), title: "Игра престолов (prequel)", isDone: false},
+    ],
+  }
+}
+
+const storybookStore = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    todoLists: todoListsReducer
+  },
+  preloadedState: initialGlobalState,
+})
 
 export const ReduxStoreProviderDecorator = (storyFn: any) => {
-  return <Provider store={store}>{storyFn()}</Provider>
+  return <Provider store={storybookStore}>{storyFn()}</Provider>
 }
